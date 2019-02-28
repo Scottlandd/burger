1885a1579436ae74169af6905e40fa7dfe6ac36a
Scott Bratcher <scottykinns@Scotts-MBP.richmond.edu>
2019-02-28 18:19:47 -0500

    28Feb2019

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burger_db;
+
+INSERT INTO burgers (burger, devoured)
+VALUES ("Bleu Ribbon Burger", true);
+
+INSERT INTO burgers (burger, devoured)
+VALUES ("Spicy Bacon Cheeseburger", false);
+
+INSERT INTO burgers (burger, devoured)
+VALUES ("Blueberry Garlic Aeoli Burger", true);
